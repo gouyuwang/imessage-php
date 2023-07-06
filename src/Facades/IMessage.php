c82b747b85afc71aa2787b14007ae4d81894b06a
ywg <529156563@qq.com>
2023-07-06 17:09:50 +0800

    add doc

--- src/Facades/IMessage.php
+++ src/Facades/IMessage.php
@@ -3,6 +3,12 @@ namespace Gouyuwang\IMessage\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method array|mixed broadcast($channel, $payload, bool $guest = false)
+ * @method array|mixed subscribe($channel, $hooks, bool $guest = false)
+ * @method array|mixed unsubscribe($channel)
+ * @method array|mixed config($channel)
+ */
 class IMessage extends Facade
 {
     /**
